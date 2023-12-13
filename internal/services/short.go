c149f8b7cfe7e5eadcde3a34f93b025a33c4af86
thebluespecs <ayushsara12@gmail.com>
2023-12-13 19:25:15 +0530

    added services

---
+++ internal/services/short.go
@@ -0,0 +1,27 @@
+package services
+
+import (
+	"short-it/internal/db"
+	"short-it/internal/logger"
+	"strconv"
+)
+
+// takes the url and returns the encoded url
+func Encode (url string, dataStore db.DB) (string, error) {
+    id, err := dataStore.Save(url)
+    if err != nil {
+        return "", err
+    }
+    return encode(uint64(id)), nil
+}
+
+// takes the code and returns the decoded url
+func Decode (code string, dataStore db.DB) (string, error) {
+    id := decode(code)
+    logger.Info("decoded id: " + strconv.Itoa(int(id)))
+    url, err := dataStore.Find(int(id))
+    if err != nil {
+        return "", err
+    }
+    return url, nil
+}
