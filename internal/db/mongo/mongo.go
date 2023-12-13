c700392a063304708f0413c12dfa5ae060a84e9a
thebluespecs <ayushsara12@gmail.com>
2023-12-13 19:16:23 +0530

    added mock mongo struct

--- internal/db/mongo/mongo.go
+++ internal/db/mongo/mongo.go
@@ -1,3 +1,48 @@
 package mongo
 
-type Mongo struct{}
+import (
+	"errors"
+	"math/rand"
+	"short-it/internal/logger"
+	"strconv"
+	"sync"
+)
+
+// Mongo is a struct that implements DB interface
+type Mongo struct {
+	m map[int]string
+}
+
+var (
+	instance *Mongo
+	one      sync.Once
+)
+
+func init() {
+	one.Do(func() {
+		instance = &Mongo{m: make(map[int]string)}
+	})
+}
+
+func GetInstance() *Mongo {
+	return instance
+}
+
+// Save saves the url to the database
+func (m *Mongo) Save(url string) (int, error) {
+	// generate a random int
+	id := rand.Intn(10000)
+	logger.Info("generated id: " + strconv.Itoa(id))
+	m.m[id] = url
+	return id, nil
+}
+
+// Find finds the url from the database
+func (m *Mongo) Find(id int) (string, error) {
+	url, ok := m.m[id]
+	if !ok {
+		logger.Error("url not found")
+		return "", errors.New("url not found")
+	}
+	return url, nil
+}
